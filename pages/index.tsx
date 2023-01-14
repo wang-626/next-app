1f4bebc8803a2c16e583695bce59a4ee508b9a2b
david <62385136+wang-626@users.noreply.github.com>
2023-01-14 22:55:30 +0800

    test next/Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,6 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className="title">
+        Welcome to   <Link href="/posts/first-post">Back to home</Link>
+      </h1>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
@@ -26,7 +30,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +123,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
