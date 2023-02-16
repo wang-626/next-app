1c3d2eb32202fa18fbc70634378d511f7a6f5e5c
david <62385136+wang-626@users.noreply.github.com>
2023-02-16 23:51:01 +0800

    Add session hook

--- components/header.tsx
+++ components/header.tsx
@@ -1,47 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function Header() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const target = e.target as typeof e.target & {
-      email: { value: string };
-      password: { value: string };
-    };
-
-    const user: userType = {
-      email: target.email.value,
-      password: target.password.value,
-    };
-
-    const set = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `query{
-                  user(email:"${user.email}",password:"${user.password}"){
-                    name
-                  }
-        }`,
-      }),
-    };
-
-    try {
-      const res = await fetch(`http://127.0.0.1:4000/graphql`, set);
-      const data = await res.json();
-      console.log(data);
-    } catch (e) {
-      console.log(e);
-    }
+    signIn();
   }
-
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
     <>
-      header
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">email:</label>
-        <input type="text" id="email" name="email" />
-        <label htmlFor="password">password</label>
-        <input type="text" id="password" name="password" />
-        <button type="submit">Submit</button>
-      </form>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
   );
 }
