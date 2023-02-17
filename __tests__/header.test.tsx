748f71ba0f7c284937769bc18c6264418d2896d8
david <62385136+wang-626@users.noreply.github.com>
2023-02-17 23:03:41 +0800

    Add button test

--- __tests__/header.test.tsx
+++ __tests__/header.test.tsx
@@ -2,25 +2,43 @@ import { render, screen } from "@testing-library/react";
 import Header from "components/header";
 import "@testing-library/jest-dom";
 import { useSession, signIn, signOut } from "next-auth/react";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("next-auth/react");
 
-describe("Home", () => {
-  it("Not logged in", () => {
+describe("Not logged in header nav button", () => {
+  it("button exist", () => {
     (useSession as jest.Mock).mockReturnValue({
       data: {},
       status: "unauthenticated",
     });
     render(<Header />);
 
-    const heading = screen.getByRole("button", {
-      name: /Sign in/i,
+    const signInBtn = screen.getByRole("button", {
+      name: /signInBtn/i,
     });
 
-    expect(heading).toBeInTheDocument();
+    const searchBtn = screen.getByRole("button", {
+      name: /searchBtn/i,
+    });
+
+    expect(signInBtn).toBeInTheDocument();
+    expect(searchBtn).toBeInTheDocument();
   });
 
-  it("Logged in", () => {
+  it("click event", async () => {
+    render(<Header />);
+    const signInBtn = screen.getByRole("button", {
+      name: /signInBtn/i,
+    });
+    const user = userEvent.setup();
+    await user.click(signInBtn);
+    expect(signInBtn).toBeInTheDocument();
+  });
+});
+
+describe("logged in header nav button", () => {
+  it("button exist", () => {
     (useSession as jest.Mock).mockReturnValue({
       data: {
         user: {
@@ -33,7 +51,7 @@ describe("Home", () => {
     render(<Header />);
 
     const heading = screen.getByRole("button", {
-      name: /Sign out/i,
+      name: /signOutBtn/i,
     });
 
     expect(heading).toBeInTheDocument();
