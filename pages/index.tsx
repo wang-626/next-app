1f6ac677ba464f377c71c4ae9ed13ff8a5392bd5
david <62385136+wang-626@users.noreply.github.com>
2023-03-08 23:31:52 +0800

    Update view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,43 @@
 import { useContext } from "react";
 import { AuthContext } from "lib/context/auth";
-import Repositories from "components/repositories/repositories";
-import { fetchUserGithubOauth } from "lib/User";
-import { githubFetch } from "lib/githubApi";
-import { verifyJtwCookie } from "lib/jwt";
-import { verifyLoginToken } from "lib/User";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import img from "public/github.svg";
 
-function Home({ repositories }) {
+function Home() {
+  const router = useRouter();
   const { authenticated, setAuthenticated } = useContext(AuthContext);
+
+  function redirectGithub() {
+    router.push("/github");
+  }
+
   if (authenticated) {
     return (
-      <div>
-        <div>
-          <h1 className="text-2xl text-slate-800">儲存庫列表</h1>
+      <div className="py-4">
+        <div className="card w-96 bg-base-200 py-3 shadow-xl">
+          <figure>
+            <Image src={img} alt="Picture of the author" width={100} height={100} className="mx-auto" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Github API</h2>
+            <p>操控你的儲存庫issue，透過github graphql 實現</p>
+            <div className="card-actions justify-end">
+              <button onClick={redirectGithub} className="btn-primary btn">
+                Go
+              </button>
+            </div>
+          </div>
         </div>
-        <Repositories repositories={repositories} />
       </div>
     );
-  } else {
-    return <p>請先登入</p>;
   }
-}
-
-export async function getServerSideProps({ req, res }) {
-  if (req.cookies.loginToken) {
-    const { token } = verifyJtwCookie(req.cookies.loginToken);
-    const user = await verifyLoginToken(token);
-    const oauth = await fetchUserGithubOauth(user.id);
-    const githubApi = new githubFetch(oauth);
-    const repositories = await githubApi.getRepositories();
-
-    return {
-      props: { repositories: repositories }, // will be passed to the page component as props
-    };
-  } else {
-    return {
-      props: { repositories: null }, // will be passed to the page component as props
-    };
-  }
+  return (
+    <>
+      <p>首頁</p>
+      <p>請先登入</p>
+    </>
+  );
 }
 
 export default Home;
