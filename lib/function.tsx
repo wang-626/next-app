97d10f3d187d2df10ea4f8c3567494fdccc4369a
david <62385136+wang-626@users.noreply.github.com>
2023-03-15 22:50:38 +0800

    Add UrlParam function

--- lib/function.tsx
+++ lib/function.tsx
@@ -1,2 +1,29 @@
-export const isNumeric = (num: any) =>
+export const isNumeric = (num: unknown) =>
   (typeof num === "number" || (typeof num === "string" && num.trim() !== "")) && !isNaN(num as number);
+
+export const removeUrlParameter = (url: string) => {
+  if (url.includes("?")) {
+    return url.slice(0, url.indexOf("?"));
+  }
+  return url;
+};
+
+export const replaceUrlParam = ({
+  url,
+  paramName,
+  paramValue,
+}: {
+  url: string;
+  paramName: string;
+  paramValue: string;
+}) => {
+  if (url.includes("?")) {
+    if (url.slice(url.indexOf("?"), url.length).includes(paramName)) {
+      let re = new RegExp(String.raw`(?<=\&|\?)${paramName}(.*(?=\&|\?)|.*$)`);
+      return url.replace(re, `${paramName}=${paramValue}`);
+    }
+    return url + `&${paramName}=${paramValue}`;
+  } else {
+    return url + `?${paramName}=${paramValue}`;
+  }
+};
