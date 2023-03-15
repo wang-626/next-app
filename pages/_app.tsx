8a1c120b7e600b086c695bd496177cb5951ccb75
david <62385136+wang-626@users.noreply.github.com>
2023-03-15 22:51:05 +0800

    Add Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthContextComponent } from "lib/context/auth";
 import Header from "components/header";
+import Footer from "components/footer";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps: { message, ...pageProps } }:
           <Component {...pageProps} />
         </div>
       </div>
+      <Footer />
     </AuthContextComponent>
   );
 }
