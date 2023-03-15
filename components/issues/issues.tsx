7fb4ce8f729a6eb8b31361959c21868b42613ea4
david <62385136+wang-626@users.noreply.github.com>
2023-03-15 22:50:24 +0800

    Add issue nav

--- components/issues/issues.tsx
+++ components/issues/issues.tsx
@@ -1,4 +1,5 @@
 import Issue from "components/issues/issue";
+import Nav from "components/issues/nav";
 import Image from "next/image";
 import img from "public/confusion.png";
 
@@ -6,6 +7,7 @@ export default function Issues({ issues }: { issues: string[] }) {
   if (issues.length > 0) {
     return (
       <div className="repository border-Stone-100 rounded-md border-2 bg-white p-5">
+        <Nav />
         {issues.map((obj, i) => {
           return <Issue key={i} issue={obj} />;
         })}
@@ -14,7 +16,8 @@ export default function Issues({ issues }: { issues: string[] }) {
   } else {
     return (
       <div className="repository border-Stone-100 flex justify-center rounded-md border-2 bg-white p-5">
-        <p className="text-center">此專案還沒有issue喔&nbsp;&nbsp;</p>
+        <Nav />
+        <p className="my-auto text-center">此專案還沒有issue喔&nbsp;&nbsp;</p>
         <Image src={img} alt="Picture of the author" width={30} height={30} />
       </div>
     );
